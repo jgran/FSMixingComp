9c0ecdeb26b4580281e4fc6fa1072bab19430e44
jgran <jgran@physics.ucsb.edu>
2014-09-26 00:03:54 -0700

    bug fix in script to make old min bias sample

--- makeOldMinBias.sh
+++ makeOldMinBias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd oldMinBias/CMSSW_7_2_0_pre5/src
+cd oldMinBias/CMSSW_7_0_0/src
 cmsenv
 echo --Making 25000 MinBias events for old mixing, job running in background
 nohup cmsRun MinBias_for_GEN_mixing.py > oldMinBias.log 2>&1 &
